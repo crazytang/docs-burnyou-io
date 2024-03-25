c8ebb7370974b9dbdfa87482174343cbebec37e1
Brian <tonightCta@gmail.com>
2024-03-25 11:55:01 +0800

    update packjson config

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,6 +1,9 @@
-import {defaultTheme} from "vuepress";
+import { defaultTheme } from "@vuepress/theme-default";
+import { viteBundler } from '@vuepress/bundler-vite'
+import { defineUserConfig } from 'vuepress'
 
-export default {
+export default defineUserConfig({
+  bundler:viteBundler(),
   locales: {
     // 键名是该语言所属的子路径
     // 作为特例，默认语言可以使用 '/' 作为其路径。
@@ -26,13 +29,13 @@ export default {
           }, {
             text: 'Battle',
             link: './Battle.md'
-          },{
+          }, {
             text: 'Vote',
             link: './Vote.md'
-          },{
+          }, {
             text: 'GuessBid',
             link: './GuessBid.md'
-          },{
+          }, {
             text: '$AA',
             link: './AA.md'
           },
@@ -45,24 +48,24 @@ export default {
           {
             text: '概述',
             link: './README.md',
-/*            children: [
-              // SidebarItem
-              {
-                text: 'github',
-                link: 'https://github.com',
-                children: [],
-              },
-            ],*/
+            /*            children: [
+                          // SidebarItem
+                          {
+                            text: 'github',
+                            link: 'https://github.com',
+                            children: [],
+                          },
+                        ],*/
           }, {
             text: '對戰',
             link: './Battle.md'
-          },{
+          }, {
             text: '投票',
             link: './Vote.md'
-          },{
+          }, {
             text: '競猜',
             link: './GuessBid.md'
-          },{
+          }, {
             text: '$AA',
             link: './AA.md'
           },
@@ -72,19 +75,19 @@ export default {
     // 侧边栏数组
     // 所有页面会使用相同的侧边栏
 
-/*    navbar: [
-      // NavbarItem
-      {
-        text: 'Foo',
-        link: '/foo/',
-      },
-      // NavbarGroup
-      {
-        text: 'Group',
-        children: ['/group/foo.md', '/group/bar.md'],
-      },
-      // 字符串 - 页面文件路径
-      '/bar/README.md',
-    ],*/
+    /*    navbar: [
+          // NavbarItem
+          {
+            text: 'Foo',
+            link: '/foo/',
+          },
+          // NavbarGroup
+          {
+            text: 'Group',
+            children: ['/group/foo.md', '/group/bar.md'],
+          },
+          // 字符串 - 页面文件路径
+          '/bar/README.md',
+        ],*/
   }),
-}
+})
